88fa4bdf50f9dfb7373d8220475f5637739adbf7
jorgetovar <jorgetovar621@gmail.com>
2021-07-08 16:42:06 -0500

    Terraform

--- main.tf
+++ main.tf
@@ -3,10 +3,38 @@ provider "aws" {
   profile = "gh"
 }
 
-resource "aws_instance" "example" {
-  ami = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
+
+resource "aws_lambda_function" "epam_query" {
+  function_name = "EpamQuery"
+  s3_bucket = var.s3_bucket
+  s3_key = "v1/mape-lambda.zip"
+  handler = "main.handler"
+  runtime = "python3.7"
+  role = aws_iam_role.epam_query_lambda.arn
   tags = {
-    Name = "terraform-example"
+    "terraform" = true
   }
+}
+
+resource "aws_iam_role" "epam_query_lambda" {
+  name = "epam_query_lambda"
+  tags = {
+    "terraform" = true
+  }
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
 }
